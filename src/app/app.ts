53657f4afa585663df6d64b50432c8efd6d27d71
TshiGabrielle <gabrielletshiamala@gmail.com>
2026-08-03 13:40:24 +0200

    Cas 4 & 5 : lister et ajouter dates

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AlimentListComponent } from './app/components/aliment-list/aliment-list';
 import { AlimentFormComponent } from './app/components/aliment-form/aliment-form';
+import { DateListComponent } from './app/components/date-list/date-list';
+import { DateFormComponent } from './app/components/date-form/date-form';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AlimentListComponent, AlimentFormComponent],
+  imports: [RouterOutlet, AlimentListComponent, AlimentFormComponent, DateListComponent, DateFormComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
